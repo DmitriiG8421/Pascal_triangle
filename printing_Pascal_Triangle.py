cda2b2d70baaa63ccca497681cf3b4eb562daf41
DmitriiG8421 <golikov.dima15@gmail.com>
2024-12-24 14:45:14 +1100

    Code Completed

--- printing_Pascal_Triangle.py
+++ printing_Pascal_Triangle.py
@@ -1,8 +1,22 @@
 number_of_ones = int(input("Type a number: "))
+
+def doSomething(j):
+    return 1
+
 print("\n")
-list = [[1]]
-print(list)
+listOfLists = []
+
 for i in range(number_of_ones):
-    a = [list[i][0] + 1]
-    list.append(a)
-    print(list)
+
+    list = [] 
+    for j in range(i+1):
+        list.append(doSomething(j))   
+
+    #Second Option   list = [doSomething(j) for j in range(i+1)]   
+    listOfLists.append(list)
+
+    
+print(listOfLists)
+
+
+
