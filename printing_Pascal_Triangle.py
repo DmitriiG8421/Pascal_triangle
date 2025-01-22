eb8a33eac4457b2a6cf1445bfa4182a7a9b62842
DmitriiG8421 <golikov.dima15@gmail.com>
2025-01-22 17:03:38 +1100

    Homework finished

--- printing_Pascal_Triangle.py
+++ printing_Pascal_Triangle.py
@@ -18,18 +18,22 @@ for i in range(number_of_ones):
 
 
 
-    lenths = []
+lenths = []
 for i in listOfLists:
     lenths.append(len(i))
 max_width = max(lenths)*2-1
 
 
-help = []
+
+
 for i in listOfLists:
-    x = map(lambda a : "_".join(a),i)
-    help.append(x)
-for i in help:
-    print(list(i))
+    x = list(map(lambda a : str(a), i))
+    print("_".join(x).center(max_width," "))
+
+
+print("End")
+
+
 
 
 
