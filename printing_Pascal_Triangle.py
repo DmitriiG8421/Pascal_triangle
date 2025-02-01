dc229cb7305eaf70c3cfcfe341c31449cf96a519
DmitriiG8421 <golikov.dima15@gmail.com>
2025-02-01 13:28:53 +1100

    I finished the Pascal Triangle

--- printing_Pascal_Triangle.py
+++ printing_Pascal_Triangle.py
@@ -1,32 +1,47 @@
-# number_of_ones = int(input("Type a number: "))
-number_of_ones = 3
+number_of_rows = int(input("Type a numberof rows (atleast 1): "))
+# number_of_ones = 3
 
 def doSomething(j):
     return 1
 
 print("\n")
 listOfLists = []
+listOfLists.append([1])
 
-for i in range(number_of_ones):
-
-    list1 = [] 
-    for j in range(i+1):
-        list1.append(doSomething(j))   
-
-    #Second Option   list = [doSomething(j) for j in range(i+1)]   
-    listOfLists.append(list1)
+pascaltriangle= [[1]]
 
 
+#    1
+#   1 1
+#  1 2 1
+# 1 3 3 1
 
-lenths = []
-for i in listOfLists:
-    lenths.append(len(i))
-max_width = max(lenths)*2-1
+for i in range(1,number_of_rows):
+    previous_row = pascaltriangle[i-1]
+    next_row = [1]
+
+    for j in range(1,len(previous_row)):
+        next_row.append(previous_row[j-1]+previous_row[j])
+    
+    next_row.append(1)
+    pascaltriangle.append(next_row)
 
 
 
+# for i in range(number_of_ones):
+#     list1 = [] 
+#     for j in range(i+1):
+#         list1.append(doSomething(j))   
+#     listOfLists.append(list1)
 
-for i in listOfLists:
+
+
+lenthslist= []
+for i in pascaltriangle:
+    lenthslist.append(len(i))
+max_width = max(lenthslist)*2-1
+
+for i in pascaltriangle:
     x = list(map(lambda a : str(a), i))
     print("_".join(x).center(max_width," "))
 
